82c0407fc595f8e826a61506028f13f424f9beea
Matthieu Drula <matthieu.drula.hs@gmail.com>
2018-01-22 10:22:43 -0400

    all working

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,5 +1,25 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+declare var Vue: any;
 
-import { AppModule } from './app.module';
+import * as GeolocationService from '@ionic-native/geolocation';
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+const geolocation = new GeolocationService.Geolocation();
+
+Vue.config.ignoredElements = ['ion-card', 'ion-card-content'];
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    loadingPosition: false,
+    geolocation: {}
+  },
+  methods: {
+    getPosition: function() {
+      this.loadingPosition = true;
+
+      geolocation.getCurrentPosition().then(({ coords })=> {
+        this.loadingPosition = false;
+        this.geolocation = coords;
+      })
+    }
+  }
+})
